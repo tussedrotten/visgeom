68dc0321723a03612813910bbc93be684c5136f1
Trym Vegard Haavardsholm <trym.haavardsholm@ntnu.no>
2023-05-25 12:48:59 +0200

    Scale box along dimensions, remove fixed matplotlib version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='visgeom',
-    version='0.1.3',
+    version='0.2.0',
     description='A simple visualisation library for 3D poses and cameras in python',
     url='https://github.com/tussedrotten/visgeom',
     author='Trym Vegard Haavardsholm',
     license='BSD-3-Clause',
     packages=['visgeom'],
-    install_requires=['numpy', 'matplotlib==3.3.1']
+    install_requires=['numpy', 'matplotlib']
 )
